1803eba82b61a5d09023b90bce0e1d978988f8f5
TouhedurRahman <touhedur.cse@gmail.com>
2023-10-31 19:21:29 +0600

    navbar, banner & footer added

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,29 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+const Banner = () => {
+    return (
+        <Carousel className="text-center">
+            <div>
+                <img src="../../../../src/assets/home/01.jpg" />
+            </div>
+            <div>
+                <img src="../../../../src/assets/home/02.jpg" />
+            </div>
+            <div>
+                <img src="../../../../src/assets/home/03.png" />
+            </div>
+            <div>
+                <img src="../../../../src/assets/home/04.jpg" />
+            </div>
+            <div>
+                <img src="../../../../src/assets/home/05.png" />
+            </div>
+            <div>
+                <img src="../../../../src/assets/home/06.png" />
+            </div>
+        </Carousel>
+    );
+};
+
+export default Banner;
