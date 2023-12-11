5fa701a0f10327ff5406c6f93a93332b39b3bdd0
TouhedurRahman <touhedur.cse@gmail.com>
2023-12-11 20:28:07 +0600

    login and logout functionality added in authprovider

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,15 +1,46 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { app } from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
+
+const auth = getAuth(app);
 
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+    const logIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(email, password);
+    }
+
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log('Current user: ', currentUser);
+            setLoading(false);
+        });
+        return () => {
+            return unsubscribe();
+        }
+    }, []);
+
     const authInfo = {
         user,
-        loading
+        loading,
+        createUser,
+        logIn,
+        logOut
     }
 
     return (
